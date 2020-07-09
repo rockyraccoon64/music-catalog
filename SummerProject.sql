8feec92c32096530a9d3f5b41011bc34c65cb084
Andrey S <norwegianwood409@gmail.com>
2020-07-09 15:23:17 +0500

    Finished adding data

--- SummerProject.sql
+++ SummerProject.sql
@@ -2,6 +2,10 @@ DROP DATABASE SummerProject;
 CREATE DATABASE SummerProject;
 USE SummerProject;
 
+/************************************************
+ *				Создание таблиц			    	*
+ ***********************************************/
+
 CREATE TABLE Bands (
 	ID INT NOT NULL AUTO_INCREMENT,
     Name NVARCHAR(50) NOT NULL,
@@ -56,6 +60,10 @@ CREATE TABLE MusicianInstrument (
     InstrumentID INT NOT NULL REFERENCES Instruments (ID),
     PRIMARY KEY (MusicianID, InstrumentID)
 );
+
+/************************************************
+ *				Заполнение таблиц		    	*
+ ***********************************************/
 
 INSERT INTO Instruments (Name) VALUES
 (N'Гитара'),
@@ -193,7 +201,7 @@ INSERT INTO Albums (Band, Name, ReleaseDate, Genre) VALUES
 (@kinks, N'Face to Face', '1966-10-28', @baroque),
 (@kinks, N'Something Else by the Kinks', '1967-09-15', @baroque),
 (@kinks, N'The Kinks Are the Village Green Preservation Society', '1968-11-22', @baroque),
-(@kinks, N'Arthur (Or the Decline and Fall of the British Empire', '1969-10-10', @rock),
+(@kinks, N'Arthur (Or the Decline and Fall of the British Empire)', '1969-10-10', @rock),
 (@kinks, N'Lola Versus Powerman and the Moneygoround, Part One', '1970-11-27', @rock),
 (@kinks, N'Muswell Hillbillies', '1971-11-24', @countryrock);
 
@@ -217,12 +225,66 @@ SELECT @beatles_tour:=ID FROM Albums
 WHERE Name = N'Magical Mystery Tour';
 SELECT @beatles_white:=ID FROM Albums
 WHERE Name = N'The Beatles';
-SELECT @beatles_sub:=ID From Albums
+SELECT @beatles_sub:=ID FROM Albums
 WHERE Name = N'Yellow Submarine';
-SELECT @beatles_abbey:=ID From Albums
+SELECT @beatles_abbey:=ID FROM Albums
 WHERE Name = N'Abbey Road';
-SELECT @beatles_let:=ID From Albums
+SELECT @beatles_let:=ID FROM Albums
 WHERE Name = N'Let It Be';
+
+SELECT @byrds_tambourine:= ID FROM Albums
+WHERE Name = N'Mr. Tambourine Man';
+SELECT @byrds_turn:= ID FROM Albums
+WHERE Name = N'Turn! Turn! Turn!';
+SELECT @byrds_5d:= ID FROM Albums
+WHERE Name = N'Fifth Dimension';
+SELECT @byrds_younger:= ID FROM Albums
+WHERE Name = N'Younger Than Yesterday';
+SELECT @byrds_brothers:= ID FROM Albums
+WHERE Name = N'The Notorious Byrd Brothers';
+SELECT @byrds_rodeo:= ID FROM Albums
+WHERE Name = N'Sweetheart of the Rodeo';
+SELECT @byrds_hyde:= ID FROM Albums
+WHERE Name = N'Dr. Byrds & Mr. Hyde';
+SELECT @byrds_byrds:= ID FROM Albums
+WHERE Name = N'Byrds';
+
+SELECT @band_pink:= ID FROM Albums
+WHERE Name = N'Music from Big Pink';
+SELECT @band_band:= ID FROM Albums
+WHERE Name = N'The Band';
+SELECT @band_fright:= ID FROM Albums
+WHERE Name = N'Stage Fright';
+SELECT @band_cahoots:= ID FROM Albums
+WHERE Name = N'Cahoots';
+SELECT @band_lights:= ID FROM Albums
+WHERE Name = N'Northern Lights - Southern Cross';
+
+SELECT @ccr_ccr:= ID FROM Albums
+WHERE Name = N'Creedence Clearwater Revival';
+SELECT @ccr_bayou:= ID FROM Albums
+WHERE Name = N'Bayou Country';
+SELECT @ccr_river:= ID FROM Albums
+WHERE Name = N'Green River';
+SELECT @ccr_willy:= ID FROM Albums
+WHERE Name = N'Willy and the Poor Boys';
+SELECT @ccr_cosmo:= ID FROM Albums
+WHERE Name = N'Cosmo''s Factory';
+SELECT @ccr_pendulum:= ID FROM Albums
+WHERE Name = N'Pendulum';
+
+SELECT @kinks_face:= ID FROM Albums
+WHERE Name = N'Face to Face';
+SELECT @kinks_something:= ID FROM Albums
+WHERE Name = N'Something Else by the Kinks';
+SELECT @kinks_village:= ID FROM Albums
+WHERE Name = N'The Kinks Are the Village Green Preservation Society';
+SELECT @kinks_arthur:= ID FROM Albums
+WHERE Name = N'Arthur (Or the Decline and Fall of the British Empire)';
+SELECT @kinks_lola:= ID FROM Albums
+WHERE Name = N'Lola Versus Powerman and the Moneygoround, Part One';
+SELECT @kinks_muswell:= ID FROM Albums
+WHERE Name = N'Muswell Hillbillies';
 
 INSERT INTO Songs (Album, Name, TrackNo) VALUES
 (@beatles_please, N'I Saw Her Standing There', 1),
@@ -429,6 +491,454 @@ INSERT INTO Songs (Album, Name, TrackNo) VALUES
 (@beatles_let, N'One After 909', 9),
 (@beatles_let, N'The Long and Winding Road', 10),
 (@beatles_let, N'For You Blue', 11),
-(@beatles_let, N'Get Back', 12);
+(@beatles_let, N'Get Back', 12),
 
-SELECT * FROM Songs;
+(@byrds_tambourine, N'Mr. Tambourine Man', 1),
+(@byrds_tambourine, N'I''ll Feel a Whole Lot Better', 2),
+(@byrds_tambourine, N'Spanish Harlem Incident', 3),
+(@byrds_tambourine, N'You Won''t Have to Cry', 4),
+(@byrds_tambourine, N'Here Without You', 5),
+(@byrds_tambourine, N'The Bells of Rhymney', 6),
+(@byrds_tambourine, N'All I Really Want to Do', 7),
+(@byrds_tambourine, N'I Knew I''d Want You', 8),
+(@byrds_tambourine, N'It''s No Use', 9),
+(@byrds_tambourine, N'Don''t Doubt Yourself, Babe', 10),
+(@byrds_tambourine, N'Chimes of Freedom', 11),
+(@byrds_tambourine, N'We''ll Meet Again', 12),
+
+(@byrds_turn, N'Turn! Turn! Turn! (To Everything There is a Season)', 1),
+(@byrds_turn, N'It Won''t Be Wrong', 2),
+(@byrds_turn, N'Set You Free This Time', 3),
+(@byrds_turn, N'Lay Down Your Weary Tune', 4),
+(@byrds_turn, N'He Was a Friend of Mine', 5),
+(@byrds_turn, N'The World Turns All Around Her', 6),
+(@byrds_turn, N'Satisfied Mind', 7),
+(@byrds_turn, N'If You''re Gone', 8),
+(@byrds_turn, N'The Times They Are a-Changin''', 9),
+(@byrds_turn, N'Wait and See', 10),
+(@byrds_turn, N'Oh! Susannah', 11),
+
+(@byrds_5d, N'5D (Fifth Dimension', 1),
+(@byrds_5d, N'Wild Mountain Thyme', 2),
+(@byrds_5d, N'Mr. Spaceman', 3),
+(@byrds_5d, N'I See You', 4),
+(@byrds_5d, N'What''s Happening?!?!', 5),
+(@byrds_5d, N'I Come and Stand at Every Door', 6),
+(@byrds_5d, N'Eight Miles High', 7),
+(@byrds_5d, N'Hey Joe (Where You Gonna Go)', 8),
+(@byrds_5d, N'Captain Soul', 9),
+(@byrds_5d, N'John Riley', 10),
+(@byrds_5d, N'2-4-2 Fox Trot (The Lear Jet Song)', 11),
+
+(@byrds_younger, N'So You Want to Be a Rock''n''Roll Star', 1),
+(@byrds_younger, N'Have You Seen Her Face', 2),
+(@byrds_younger, N'C.T.A.-102', 3),
+(@byrds_younger, N'Renaissance Fair', 4),
+(@byrds_younger, N'Time Between', 5),
+(@byrds_younger, N'Everybody''s Been Burned', 6),
+(@byrds_younger, N'Thoughts and Words', 7),
+(@byrds_younger, N'Mind Gardens', 8),
+(@byrds_younger, N'My Back Pages', 9),
+(@byrds_younger, N'The Girl with No Name', 10),
+(@byrds_younger, N'Why', 11),
+
+(@byrds_brothers, N'Artificial Energy', 1),
+(@byrds_brothers, N'Goin'' Back', 2),
+(@byrds_brothers, N'Natural Harmony', 3),
+(@byrds_brothers, N'Draft Morning', 4),
+(@byrds_brothers, N'Wasn''t Born to Follow', 5),
+(@byrds_brothers, N'Get to You', 6),
+(@byrds_brothers, N'Change Is Now', 7),
+(@byrds_brothers, N'Old John Robertson', 8),
+(@byrds_brothers, N'Tribal Gathering', 9),
+(@byrds_brothers, N'Dolphin''s Smile', 10),
+(@byrds_brothers, N'Space Odyssey', 11),
+
+(@byrds_rodeo, N'You Ain''t Goin'' Nowhere', 1),
+(@byrds_rodeo, N'I Am a Pilgrim', 2),
+(@byrds_rodeo, N'The Christian Life', 3),
+(@byrds_rodeo, N'You Don''t Miss Your Water', 4),
+(@byrds_rodeo, N'You''re Still on My Mind', 5),
+(@byrds_rodeo, N'Pretty Boy Floyd', 6),
+(@byrds_rodeo, N'Hickory Wind', 7),
+(@byrds_rodeo, N'One Hundred Years from Now', 8),
+(@byrds_rodeo, N'Blue Canadian Rockies', 9),
+(@byrds_rodeo, N'Life in Prison', 10),
+(@byrds_rodeo, N'Nothing Was Delivered', 11),
+
+(@byrds_hyde, N'This Wheel''s on Fire', 1),
+(@byrds_hyde, N'Old Blue', 2),
+(@byrds_hyde, N'Your Gentle Way of Loving Me', 3),
+(@byrds_hyde, N'Child of the Universe', 4),
+(@byrds_hyde, N'Nashville West', 5),
+(@byrds_hyde, N'Drug Store Truck Drivin'' Man', 6),
+(@byrds_hyde, N'King Apathy III', 7),
+(@byrds_hyde, N'Candy', 8),
+(@byrds_hyde, N'Bad Night at the Whiskey', 9),
+(@byrds_hyde, N'Medley: My Back Pages/B.J. Blues/Baby What You Want Me to Do', 10),
+
+(@byrds_byrds, N'Full Circle', 1),
+(@byrds_byrds, N'Sweet Mary', 2),
+(@byrds_byrds, N'Changing Heart', 3),
+(@byrds_byrds, N'For Free', 4),
+(@byrds_byrds, N'Born to Rock ''n'' Roll', 5),
+(@byrds_byrds, N'Things Will Be Better', 6),
+(@byrds_byrds, N'Cowgirl in the Sand', 7),
+(@byrds_byrds, N'Long Live the King', 8),
+(@byrds_byrds, N'Borrowing Time', 9),
+(@byrds_byrds, N'Laughing', 10),
+(@byrds_byrds, N'(See the Sky) About to Rain', 11),
+
+(@band_pink, N'Tears of Rage', 1),
+(@band_pink, N'To Kingdom Come', 2),
+(@band_pink, N'In a Station', 3),
+(@band_pink, N'Caledonia Mission', 4),
+(@band_pink, N'The Weight', 5),
+(@band_pink, N'We Can Talk', 6),
+(@band_pink, N'Long Black Veil', 7),
+(@band_pink, N'Chest Fever', 8),
+(@band_pink, N'Lonesome Suzie', 9),
+(@band_pink, N'This Wheel''s on Fire', 10),
+(@band_pink, N'I Shall Be Released', 11),
+
+(@band_band, N'Across the Great Divide', 1),
+(@band_band, N'Rag Mama Rag', 2),
+(@band_band, N'The Night They Drove Old Dixie Down', 3),
+(@band_band, N'When You Awake', 4),
+(@band_band, N'Up on Cripple Creek', 5),
+(@band_band, N'Whispering Pines', 6),
+(@band_band, N'Jemima Surrender', 7),
+(@band_band, N'Rockin'' Chair', 8),
+(@band_band, N'Look Out Cleveland', 9),
+(@band_band, N'Jawbone', 10),
+(@band_band, N'The Unfaithful Servant', 11),
+(@band_band, N'King Harvest (Has Surely Come)', 12),
+
+(@band_fright, N'Strawberry Wine', 1),
+(@band_fright, N'Sleeping', 2),
+(@band_fright, N'Time to Kill', 3),
+(@band_fright, N'Just Another Whistle Stop', 4),
+(@band_fright, N'All La Glory', 5),
+(@band_fright, N'The Shape I''m In', 6),
+(@band_fright, N'The W.S. Walcott Medicine Show', 7),
+(@band_fright, N'Daniel and the Sacred Harp', 8),
+(@band_fright, N'Stage Fright', 9),
+(@band_fright, N'The Rumor', 10),
+
+(@band_cahoots, N'Life Is a Carnival', 1),
+(@band_cahoots, N'When I Paint My Masterpiece', 2),
+(@band_cahoots, N'Last of the Blacksmiths', 3),
+(@band_cahoots, N'Where Do We Go From Here?', 4),
+(@band_cahoots, N'4% Pantomime', 5),
+(@band_cahoots, N'Shoot Out in Chinatown', 6),
+(@band_cahoots, N'The Moon Struck One', 7),
+(@band_cahoots, N'Thinkin'' Out Loud', 8),
+(@band_cahoots, N'Smoke Signal', 9),
+(@band_cahoots, N'Volcano', 10),
+(@band_cahoots, N'The River Hymn', 11),
+
+(@band_lights, N'Forbidden Fruit', 1),
+(@band_lights, N'Hobo Jungle', 2),
+(@band_lights, N'Ophelia', 3),
+(@band_lights, N'Acadian Driftwood', 4),
+(@band_lights, N'Ring Your Bell', 5),
+(@band_lights, N'It Makes No Difference', 6),
+(@band_lights, N'Jupiter Hollow', 7),
+(@band_lights, N'Rags and Bones', 8),
+
+(@ccr_ccr, N'I Put a Spell on You', 1),
+(@ccr_ccr, N'The Working Man', 2),
+(@ccr_ccr, N'Suzie Q', 3),
+(@ccr_ccr, N'Ninety-Nine and a Half (Won''t Do)', 4),
+(@ccr_ccr, N'Get Down Woman', 5),
+(@ccr_ccr, N'Porterville', 6),
+(@ccr_ccr, N'Gloomy', 7),
+(@ccr_ccr, N'Walk on the Water', 8),
+
+(@ccr_bayou, N'Born on the Bayou', 1),
+(@ccr_bayou, N'Bootleg', 2),
+(@ccr_bayou, N'Graveyard Train', 3),
+(@ccr_bayou, N'Good Golly, Miss Molly', 4),
+(@ccr_bayou, N'Penthouse Pauper', 5),
+(@ccr_bayou, N'Proud Mary', 6),
+(@ccr_bayou, N'Keep on Chooglin''', 7),
+
+(@ccr_river, N'Green River', 1),
+(@ccr_river, N'Commotion', 2),
+(@ccr_river, N'Tombstone Shadow', 3),
+(@ccr_river, N'Wrote a Song for Everyone', 4),
+(@ccr_river, N'Bad Moon Rising', 5),
+(@ccr_river, N'Lodi', 6),
+(@ccr_river, N'Cross-Tie Walker', 7),
+(@ccr_river, N'Sinister Purpose', 8),
+(@ccr_river, N'The Night Time Is the Right Time', 9),
+
+(@ccr_willy, N'Down on the Corner', 1),
+(@ccr_willy, N'It Came Out of the Sky', 2),
+(@ccr_willy, N'Cotton Fields', 3),
+(@ccr_willy, N'Poorboy Shuffle', 4),
+(@ccr_willy, N'Feelin'' Blue', 5),
+(@ccr_willy, N'Fortunate Son', 6),
+(@ccr_willy, N'Don''t Look Now (It Ain''t You or Me)', 7),
+(@ccr_willy, N'The Midnight Special', 8),
+(@ccr_willy, N'Side o'' the Road', 9),
+(@ccr_willy, N'Effigy', 10),
+
+(@ccr_cosmo, N'Ramble Tamble', 1),
+(@ccr_cosmo, N'Before You Accuse Me', 2),
+(@ccr_cosmo, N'Travelin'' Band', 3),
+(@ccr_cosmo, N'Ooby Dooby', 4),
+(@ccr_cosmo, N'Lookin'' Out My Back Door', 5),
+(@ccr_cosmo, N'Run Through the Jungle', 6),
+(@ccr_cosmo, N'Up Around the Bend', 7),
+(@ccr_cosmo, N'My Baby Left Me', 8),
+(@ccr_cosmo, N'Who''ll Stop the Rain', 9),
+(@ccr_cosmo, N'I Heard It Through the Grapevine', 10),
+(@ccr_cosmo, N'Long as I Can See the Light', 11),
+
+(@ccr_pendulum, N'Pagan Baby', 1),
+(@ccr_pendulum, N'Sailor''s Lament', 2),
+(@ccr_pendulum, N'Chameleon', 3),
+(@ccr_pendulum, N'Have You Ever Seen the Rain', 4),
+(@ccr_pendulum, N'(Wish I Could) Hideaway', 5),
+(@ccr_pendulum, N'Born to Move', 6),
+(@ccr_pendulum, N'Hey Tonight', 7),
+(@ccr_pendulum, N'It''s Just a Thought', 8),
+(@ccr_pendulum, N'Molina', 9),
+(@ccr_pendulum, N'Rude Awakening #2', 10),
+
+(@kinks_face, N'Party Line', 1),
+(@kinks_face, N'Rosy Won''t You Please Come Home', 2),
+(@kinks_face, N'Dandy', 3),
+(@kinks_face, N'Too Much on My Mind', 4),
+(@kinks_face, N'Session Man', 5),
+(@kinks_face, N'Rainy Day in June', 6),
+(@kinks_face, N'A House in the Country', 7),
+(@kinks_face, N'Holiday in Waikiki', 8),
+(@kinks_face, N'Most Exclusive Residence for Sale', 9),
+(@kinks_face, N'Fancy', 10),
+(@kinks_face, N'Little Miss Queen of Darkness', 11),
+(@kinks_face, N'You''re Lookin'' Fine', 12),
+(@kinks_face, N'Sunny Afternoon', 13),
+(@kinks_face, N'I''ll Remember', 14),
+
+(@kinks_something, N'David Watts', 1),
+(@kinks_something, N'Death of a Clown', 2),
+(@kinks_something, N'Two Sisters', 3),
+(@kinks_something, N'No Return', 4),
+(@kinks_something, N'Harry Rag', 5),
+(@kinks_something, N'Tin Soldier Man', 6),
+(@kinks_something, N'Situation Vacant', 7),
+(@kinks_something, N'Love Me Till the Sun Shines', 8),
+(@kinks_something, N'Lazy Old Sun', 9),
+(@kinks_something, N'Afternoon Tea', 10),
+(@kinks_something, N'Funny Face', 11),
+(@kinks_something, N'End of the Season', 12),
+(@kinks_something, N'Waterloo Sunset', 13),
+
+(@kinks_village, N'The Village Green Preservation Society', 1),
+(@kinks_village, N'Do You Remember Walter?', 2),
+(@kinks_village, N'Picture Book', 3),
+(@kinks_village, N'Johnny Thunder', 4),
+(@kinks_village, N'Last of the Steam-Powered Trains', 5),
+(@kinks_village, N'Big Sky', 6),
+(@kinks_village, N'Sitting by the Riverside', 7),
+(@kinks_village, N'Animal Farm', 8),
+(@kinks_village, N'Village Green', 9),
+(@kinks_village, N'Starstruck', 10),
+(@kinks_village, N'Phenomenal Cat', 11),
+(@kinks_village, N'All of My Friends Were There', 12),
+(@kinks_village, N'Wicked Annabella', 13),
+(@kinks_village, N'Monica', 14),
+(@kinks_village, N'People Take Pictures of Each Other', 15),
+
+(@kinks_arthur, N'Victoria', 1),
+(@kinks_arthur, N'Yes Sir, No Sir', 2),
+(@kinks_arthur, N'Some Mother''s Son', 3),
+(@kinks_arthur, N'Drivin''', 4),
+(@kinks_arthur, N'Brainwashed', 5),
+(@kinks_arthur, N'Australia', 6),
+(@kinks_arthur, N'Shangri-La', 7),
+(@kinks_arthur, N'Mr. Churchill Says', 8),
+(@kinks_arthur, N'She''s Bought a Hat Like Princess Marina', 9),
+(@kinks_arthur, N'Young and Innocent Days', 10),
+(@kinks_arthur, N'Nothing to Say', 11),
+(@kinks_arthur, N'Arthur', 12),
+
+(@kinks_lola, N'The Contenders', 1),
+(@kinks_lola, N'Strangers', 2),
+(@kinks_lola, N'Denmark Street', 3),
+(@kinks_lola, N'Get Back in Line', 4),
+(@kinks_lola, N'Lola', 5),
+(@kinks_lola, N'Top of the Pops', 6),
+(@kinks_lola, N'The Moneygoround', 7),
+(@kinks_lola, N'This Time Tomorrow', 8),
+(@kinks_lola, N'A Long Way from Home', 9),
+(@kinks_lola, N'Rats', 10),
+(@kinks_lola, N'Apeman', 11),
+(@kinks_lola, N'Powerman', 12),
+(@kinks_lola, N'Got to Be Free', 13),
+
+(@kinks_muswell, N'20th Century Man', 1),
+(@kinks_muswell, N'Acute Schizophrenia Paranoia Blues', 2),
+(@kinks_muswell, N'Holiday', 3),
+(@kinks_muswell, N'Skin and Bone', 4),
+(@kinks_muswell, N'Alcohol', 5),
+(@kinks_muswell, N'Complicated Life', 6),
+(@kinks_muswell, N'Here Come the People in Grey', 7),
+(@kinks_muswell, N'Have a Cuppa Tea', 8),
+(@kinks_muswell, N'Holloway Jail', 9),
+(@kinks_muswell, N'Oklahoma U.S.A.', 10),
+(@kinks_muswell, N'Uncle Son', 11),
+(@kinks_muswell, N'Muswell Hillbilly', 12);
+
+SELECT @guitar:=ID FROM Instruments
+WHERE Name = N'Гитара';
+SELECT @keys:=ID FROM Instruments
+WHERE Name = N'Клавишные';
+SELECT @drums:=ID FROM Instruments
+WHERE Name = N'Ударные';
+SELECT @bass:=ID FROM Instruments
+WHERE Name = N'Бас-гитара';
+SELECT @vocals:=ID FROM Instruments
+WHERE Name = N'Вокал';
+SELECT @backvox:=ID FROM Instruments
+WHERE Name = N'Бэк-вокал';
+SELECT @harmonica:=ID FROM Instruments
+WHERE Name = N'Губная гармоника';
+SELECT @sax:=ID FROM Instruments
+WHERE Name = N'Саксофон';
+SELECT @dbass:=ID FROM Instruments
+WHERE Name = N'Контрабас';
+SELECT @accordion:=ID FROM Instruments
+WHERE Name = N'Аккордеон';
+SELECT @banjo:=ID FROM Instruments
+WHERE Name = N'Банджо';
+SELECT @sitar:=ID FROM Instruments
+WHERE Name = N'Ситар';
+
+SELECT @lennon:=ID FROM Musicians
+WHERE Name = N'John Lennon';
+SELECT @macca:=ID FROM Musicians
+WHERE Name = N'Paul McCartney';
+SELECT @harrison:=ID FROM Musicians
+WHERE Name = N'George Harrison';
+SELECT @starr:=ID FROM Musicians
+WHERE Name = N'Ringo Starr';
+
+SELECT @mcguinn:=ID FROM Musicians
+WHERE Name = N'Roger McGuinn';
+SELECT @clark:=ID FROM Musicians
+WHERE Name = N'Gene Clark';
+SELECT @crosby:=ID FROM Musicians
+WHERE Name = N'David Crosby';
+SELECT @clarke:=ID FROM Musicians
+WHERE Name = N'Michael Clarke';
+SELECT @hillman:=ID FROM Musicians
+WHERE Name = N'Chris Hillman';
+
+
+SELECT @danko:=ID FROM Musicians
+WHERE Name = N'Rick Danko';
+SELECT @helm:=ID FROM Musicians
+WHERE Name = N'Levon Helm';
+SELECT @hudson:=ID FROM Musicians
+WHERE Name = N'Garth Hudson';
+SELECT @manuel:=ID FROM Musicians
+WHERE Name = N'Richard Manuel';
+SELECT @robertson:=ID FROM Musicians
+WHERE Name = N'Robbie Robertson';
+
+SELECT @jfogerty:=ID FROM Musicians
+WHERE Name = N'John Fogerty';
+SELECT @tfogerty:=ID FROM Musicians
+WHERE Name = N'Tom Fogerty';
+SELECT @cook:=ID FROM Musicians
+WHERE Name = N'Stu Cook';
+SELECT @clifford:=ID FROM Musicians
+WHERE Name = N'Doug Clifford';
+
+SELECT @rdavies:=ID FROM Musicians
+WHERE Name = N'Ray Davies';
+SELECT @ddavies:=ID FROM Musicians
+WHERE Name = N'Dave Davies';
+SELECT @avory:=ID FROM Musicians
+WHERE Name = N'Mick Avory';
+SELECT @quaife:=ID FROM Musicians
+WHERE Name = N'Pete Quaife';
+
+INSERT INTO MusicianInstrument (MusicianID, InstrumentID) VALUES
+(@lennon, @guitar),
+(@lennon, @keys),
+(@lennon, @harmonica),
+(@lennon, @vocals),
+(@lennon, @bass),
+(@macca, @vocals),
+(@macca, @bass),
+(@macca, @guitar),
+(@macca, @keys),
+(@macca, @drums),
+(@harrison, @vocals),
+(@harrison, @guitar),
+(@harrison, @sitar),
+(@harrison, @keys),
+(@harrison, @bass),
+(@starr, @drums),
+(@starr, @vocals),
+
+(@mcguinn, @guitar),
+(@mcguinn, @banjo),
+(@mcguinn, @keys),
+(@mcguinn, @vocals),
+(@clark, @guitar),
+(@clark, @harmonica),
+(@clark, @vocals),
+(@crosby, @guitar),
+(@crosby, @vocals),
+(@clarke, @drums),
+(@hillman, @bass),
+(@hillman, @guitar),
+(@hillman, @vocals),
+
+(@danko, @bass),
+(@danko, @vocals),
+(@danko, @dbass),
+(@danko, @guitar),
+(@helm, @drums),
+(@helm, @vocals),
+(@helm, @guitar),
+(@hudson, @keys),
+(@hudson, @accordion),
+(@hudson, @sax),
+(@manuel, @keys),
+(@manuel, @drums),
+(@manuel, @vocals),
+(@robertson, @guitar),
+(@robertson, @vocals),
+
+(@clifford, @drums),
+(@clifford, @backvox),
+(@cook, @bass),
+(@cook, @backvox),
+(@jfogerty, @guitar),
+(@jfogerty, @vocals),
+(@jfogerty, @keys),
+(@jfogerty, @harmonica),
+(@jfogerty, @sax),
+(@tfogerty, @guitar),
+(@tfogerty, @backvox),
+
+(@rdavies, @vocals),
+(@rdavies, @backvox),
+(@rdavies, @guitar),
+(@rdavies, @keys),
+(@rdavies, @harmonica),
+(@ddavies, @guitar),
+(@ddavies, @vocals),
+(@ddavies, @backvox),
+(@ddavies, @keys),
+(@avory, @drums),
+(@quaife, @bass),
+(@quaife, @backvox);
+
+SELECT * FROM MusicianInstrument;
