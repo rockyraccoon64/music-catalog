6fef1b282269cadb5e1df51aabb21c7fc4d184d5
Andrey S <norwegianwood409@gmail.com>
2020-07-17 14:27:37 +0500

    First GUI successes

--- src/music/database/MusicApp.java
+++ src/music/database/MusicApp.java
@@ -3,19 +3,32 @@ package music.database;
 import music.database.items.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.HashMap;
 
-public class MusicApp extends Frame implements WindowListener {
+public class MusicApp extends Frame implements WindowListener, ActionListener {
 
     public MusicApp() {
         setLayout(new BorderLayout());
 
         setTitle("Музыкальная база данных");
         setSize(1000, 750);
+        setBackground(new Color(255, 244, 161));
+
+        Panel topPanel = new Panel(new BorderLayout());
+
+        Label mainLabel = new Label("Музыкальная база данных");
+        mainLabel.setAlignment(Label.CENTER);
+        mainLabel.setFont(new Font("Serif", Font.BOLD, 30));
+        topPanel.add(mainLabel, BorderLayout.CENTER);
+
+        Panel paddingPanel = new Panel();
+        paddingPanel.setSize(0, 50);
+        topPanel.add(paddingPanel, BorderLayout.NORTH);
+
+        add(topPanel, BorderLayout.NORTH);
 
         addWindowListener(this);
 
@@ -41,4 +54,9 @@ public class MusicApp extends Frame implements WindowListener {
     @Override public void windowDeiconified(WindowEvent evt) { System.out.println("Window Deiconified"); }
     @Override public void windowActivated(WindowEvent evt) { System.out.println("Window Activated"); }
     @Override public void windowDeactivated(WindowEvent evt) { System.out.println("Window Deactivated"); }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
